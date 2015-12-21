2eb0d58937344f6185a803d6ba47a29c3ff0eff2
Andrew <milichab@gmail.com>
2015-12-21 10:10:12 -0500

    Example code to test

--- XBee_Test.py
+++ XBee_Test.py
@@ -1,11 +1,65 @@
+import sys, serial, argparse
+from time import sleep
+from collections import deque
 from xbee import XBee,ZigBee
-import serial
+import numpy as np
+import matplotlib.pyplot as plt 
+import matplotlib.animation as animation
 
 """
 	XBee communication and datalogging via Python application. 
+
+	See https://gist.github.com/electronut/d5e5f68c610821e311b0/ 
 """
 
-ser = serial.Serial('/dev/ttyUSB0', 9600) # TODO: get correct port name 
+# plot class
+class AnalogPlot:
+  	# constr
+  	def __init__(self, strPort, maxLen):
+  	    # open serial port
+  	    self.ser = serial.Serial(strPort, 9600)
+	
+  	    self.ax = deque([0.0]*maxLen)
+  	    self.ay = deque([0.0]*maxLen)
+  	    self.maxLen = maxLen
+	
+  	# add to buffer
+  	def addToBuf(self, buf, val):
+  	    if len(buf) < self.maxLen:
+  	        buf.append(val)
+  	    else:
+  	        buf.pop()
+  	        buf.appendleft(val)
+	
+  	# add data
+  	def add(self, data):
+  	    assert(len(data) == 2)
+  	    self.addToBuf(self.ax, data[0])
+  	    self.addToBuf(self.ay, data[1])
+	
+  	# update plot
+  	def update(self, frameNum, a0, a1):
+  	    try:
+  	        line = self.ser.readline()
+  	        data = [float(val) for val in line.split()]
+  	        # print data
+  	        if(len(data) == 2):
+  	            self.add(data)
+  	            a0.set_data(range(self.maxLen), self.ax)
+  	            a1.set_data(range(self.maxLen), self.ay)
+  	    except KeyboardInterrupt:
+  	        print('exiting')
+  	    
+  	    return a0, 
+	
+  	# clean up
+  	def close(self):
+  	    # close serial
+  	    self.ser.flush()
+  	    self.ser.close()    
+
+
+# ser = serial.Serial('/dev/ttyUSB0', 9600) # TODO: get correct port name 
 
 # Use an XBee 802.15.4 device
 # To use with an XBee ZigBee device, replace with:
@@ -22,4 +76,5 @@ ser = serial.Serial('/dev/ttyUSB0', 9600) # TODO: get correct port name
 # xbee.remote_at(
 #     dest_addr='\x56\x78',
 #     command='WR')
+
 
